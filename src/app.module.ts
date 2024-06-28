ab199ea81fb1e8d53e0c509fd60e5e0988244d78
Sv Wang <ifihavewings@163.com>
2024-06-28 10:06:42 +0100

    nest-app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadModule } from './upload/upload.module';
-
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 @Module({
-  imports: [UploadModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    UploadModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
